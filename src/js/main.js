7719a3909e5d3f467c8f6abe4c0cc20a1ff27f95
Troshin Sergey <serge-troshin@yandex.ru>
2021-08-30 16:24:30 +0300

    добавляет кастомный селект + кастомный чекбокс

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 //= ./../../node_modules/jquery/dist/jquery.min.js
 //= ./../../node_modules/swiper/swiper-bundle.min.js
 //= ./libs/jquery.fancybox.min.js
+//= ./libs/choices.min.js
 
 //= ./partials/main.js
 //= ./partials/swiper-init.js
