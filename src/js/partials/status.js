ce33d502cb9a2a2407e0d72d6943fe55ae647245
Troshin Sergey <serge-troshin@yandex.ru>
2021-09-09 17:02:23 +0300

    добавляет шкалу накопительной скидки

--- src/js/partials/status.js
+++ src/js/partials/status.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', () => {
   const orderStatus = document.querySelector('.__js_order-status');
+  const discountStatus = document.querySelector('.__js_discount-status');
 
   if (orderStatus) {
     const steps = orderStatus.querySelectorAll('.order-status__step');
@@ -32,5 +33,53 @@ window.addEventListener('load', () => {
       }
       
     });
+  }
+
+  if (discountStatus) {
+    const separators = discountStatus.querySelectorAll('.discount-status__sep');
+    const line = discountStatus.querySelector('.discount-status__line');
+    const thumb = discountStatus.querySelector('.discount-status__thumb');
+    const currentProgress = parseInt(discountStatus.dataset.progress, 10);
+
+    const ModifierClass = {
+      THUMB_SHIFT: 'discount-status__thumb--shifted',
+      SEP_ACTIVE: 'discount-status__sep--active',
+      SEP_DONE: 'discount-status__sep--done'
+    };
+
+    const ValidIndex = {
+      START: 2,
+      END: 23
+    };
+
+    const index = Math.floor(currentProgress / 4);
+    const lastDoneSepOffset = separators[index - 1].offsetLeft;
+    const percent = lastDoneSepOffset * 100  / discountStatus.offsetWidth;
+
+    line.style.width = percent + '%';
+
+    separators.forEach((it, i) => {
+      
+        if (i < index) {
+          it.classList.add(ModifierClass.SEP_DONE);
+        }
+      
+      
+    });
+
+    if (index >= ValidIndex.START && index <= ValidIndex.END) {
+      thumb.classList.add(ModifierClass.THUMB_SHIFT);
+      thumb.style.left = percent + '%';
+
+    } else if (index > ValidIndex.END) {
+      thumb.style.left = 'auto';
+      thumb.style.right = '0';
+    }
+
+    if (index === ValidIndex.END - 2 || index === ValidIndex.END - 1) {
+      thumb.style.bottom = '65px';
+    } else if (index >= ValidIndex.END) {
+      thumb.style.bottom = '80px';
+    }
   }
 });
